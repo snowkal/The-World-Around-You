4f089797905be200e4ad1338b37d1810c1c22c38
Snowkal <randomuselessname45@gmail.com>
2024-08-31 23:19:51 -0400

    initial

---
+++ src/main/java/net/snowkal/theway/TheWorldAroundYouClient.java
@@ -0,0 +1,10 @@
+package net.snowkal.theway;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheWorldAroundYouClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
