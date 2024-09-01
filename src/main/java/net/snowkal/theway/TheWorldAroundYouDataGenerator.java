77f0be53094cf0b0c8842ae9d24a44ab56fd7194
Snowkal <randomuselessname45@gmail.com>
2024-08-31 23:05:55 -0400

    initial

---
+++ src/main/java/net/snowkal/theway/TheWorldAroundYouDataGenerator.java
@@ -0,0 +1,11 @@
+package net.snowkal.theway;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheWorldAroundYouDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
